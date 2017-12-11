fed58d91d2b3b29427a7ac15f6947bb0ad75cbf6
Nasser Benabderrazik <benab.nasser@gmail.com>
2017-12-11 17:05:27 +0100

    Change project from Python 3 to 2

--- vqa_api/PythonHelperTools/vqaDemo.py
+++ vqa_api/PythonHelperTools/vqaDemo.py
@@ -6,14 +6,14 @@ import skimage.io as io
 import matplotlib.pyplot as plt
 import os
 
-dataDir     = r'C:\Users\Nasser Benab\Documents\git\data\vqa'
-versionType = '' # this should be '' when using VQA v1.0 dataset
-taskType    = 'OpenEnded' # 'OpenEnded' only for v2.0. 'OpenEnded' or 'MultipleChoice' for v1.0
-dataType    = 'mscoco'  # 'mscoco' only for v1.0. 'mscoco' for real and 'abstract_v002' for abstract for v1.0.
-dataSubType = 'train2014'
-annFile     = '%s/Annotations/%s%s_%s_annotations.json'%(dataDir, versionType, dataType, dataSubType)
-quesFile    = '%s/Questions/%s%s_%s_%s_questions.json'%(dataDir, versionType, taskType, dataType, dataSubType)
-imgDir      = '%s/Images/%s/%s/' %(dataDir, dataType, dataSubType)
+dataDir		='../../VQA'
+versionType ='v2_' # this should be '' when using VQA v2.0 dataset
+taskType    ='OpenEnded' # 'OpenEnded' only for v2.0. 'OpenEnded' or 'MultipleChoice' for v1.0
+dataType    ='mscoco'  # 'mscoco' only for v1.0. 'mscoco' for real and 'abstract_v002' for abstract for v1.0.
+dataSubType ='train2014'
+annFile     ='%s/Annotations/%s%s_%s_annotations.json'%(dataDir, versionType, dataType, dataSubType)
+quesFile    ='%s/Questions/%s%s_%s_%s_questions.json'%(dataDir, versionType, taskType, dataType, dataSubType)
+imgDir 		= '%s/Images/%s/%s/' %(dataDir, dataType, dataSubType)
 
 # initialize VQA api for QA annotations
 vqa=VQA(annFile, quesFile)
@@ -29,10 +29,10 @@ vqa.showQA([randomAnn])
 imgId = randomAnn['image_id']
 imgFilename = 'COCO_' + dataSubType + '_'+ str(imgId).zfill(12) + '.jpg'
 if os.path.isfile(imgDir + imgFilename):
-    I = io.imread(imgDir + imgFilename)
-    plt.imshow(I)
-    plt.axis('off')
-    plt.show()
+	I = io.imread(imgDir + imgFilename)
+	plt.imshow(I)
+	plt.axis('off')
+	plt.show()
 
 # load and display QA annotations for given answer types
 """
@@ -48,10 +48,10 @@ vqa.showQA([randomAnn])
 imgId = randomAnn['image_id']
 imgFilename = 'COCO_' + dataSubType + '_'+ str(imgId).zfill(12) + '.jpg'
 if os.path.isfile(imgDir + imgFilename):
-    I = io.imread(imgDir + imgFilename)
-    plt.imshow(I)
-    plt.axis('off')
-    plt.show()
+	I = io.imread(imgDir + imgFilename)
+	plt.imshow(I)
+	plt.axis('off')
+	plt.show()
 
 # load and display QA annotations for given images
 """
@@ -66,8 +66,8 @@ vqa.showQA([randomAnn])
 imgId = randomAnn['image_id']
 imgFilename = 'COCO_' + dataSubType + '_'+ str(imgId).zfill(12) + '.jpg'
 if os.path.isfile(imgDir + imgFilename):
-    I = io.imread(imgDir + imgFilename)
-    plt.imshow(I)
-    plt.axis('off')
-    plt.show()
+	I = io.imread(imgDir + imgFilename)
+	plt.imshow(I)
+	plt.axis('off')
+	plt.show()
 
