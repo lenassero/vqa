9c55b15e3528eef3c7e1dea28909bb560e893a8f
Nasser Benabderrazik <benab.nasser@gmail.com>
2017-12-11 22:52:04 +0100

    Add the path to question types

--- tools.py
+++ tools.py
@@ -4,16 +4,23 @@
 """Functions to use across the different methods.
 """
 
+import os
+
 def ann_file(dataDir, versionType, dataType, dataSubType):
-	filename = "{}/Annotations/{}{}_{}_annotations.json".format(dataDir, 
-		versionType, dataType, dataSubType)
+	filename = os.path.join(dataDir, "Annotations", "{}{}_{}_annotations.json".\
+		format(versionType, dataType, dataSubType))
 	return filename
 
 def ques_file(dataDir, versionType, taskType, dataType, dataSubType):
-	filename = "{}/Questions/{}{}_{}_{}_questions.json".format(dataDir, 
-		versionType, taskType, dataType, dataSubType)
+	filename = os.path.join(dataDir, "Questions", "{}{}_{}_{}_questions.json".\
+		format(versionType, taskType, dataType, dataSubType))
 	return filename
 
 def img_dir(dataDir, dataType, dataSubType):
-	dirname = "{}/Images/{}/{}/".format(dataDir, dataType, dataSubType)
+	dirname = os.path.join(dataDir, "Images", dataType, dataSubType)
 	return dirname
+
+def qtypes_file(dataDir, dataType="mscoco"):
+	filename = os.path.join(dataDir, "QuestionTypes", "{}_question_types.txt".\
+		format(dataType))
+	return filename
